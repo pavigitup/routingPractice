0d42a6352c259a68254758adf16d033697465e9d
pavigitup <pavithragokali@gmail.com>
2024-02-02 15:12:58 +0000

    v1

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -2,6 +2,13 @@ import './index.css'
 
 const NotFound = () => (
   <>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+        alt="not found"
+      />
+    </div>
+
     <h1>Not Found</h1>
   </>
 )
